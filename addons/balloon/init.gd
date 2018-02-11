7aca1d2bd463a4b3ba577ab2521718f8acd9e433
Marcos Augusto Bitetti <marcosbitetti@gmail.com>
2018-02-11 21:59:22 -0200

    add extra menu tools

--- addons/balloon/init.gd
+++ addons/balloon/init.gd
@@ -1,6 +1,11 @@
 tool
 extends EditorPlugin
 
+var _menu = MenuButton.new()
+var help
+var about
+
+
 func _enter_tree():
     add_custom_type("Balloon Text", "Control", preload("balloon.gd"), preload("assets/icon_balloon.png"))
     pass
@@ -8,3 +13,30 @@ func _enter_tree():
 func _exit_tree():
 	remove_custom_type("Balloon Text")
 	pass
+
+
+func item_selected(id):
+	var p = get_editor_interface().get_editor_viewport()
+	match id:
+		1:
+			OS.shell_open("https://raw.githubusercontent.com/marcosbitetti/godot_balloon_text/master/addons/balloon/help.html")
+		2:
+			OS.shell_open("https://raw.githubusercontent.com/marcosbitetti/godot_balloon_text/master/addons/balloon/about.html")
+	
+
+func handles(obj):
+	if not obj.get('BALLOON_TRUE'):
+		if _menu.is_inside_tree():
+			_menu.get_parent().remove_child(_menu)
+		return false
+	
+	_menu.text = 'Balloon'
+	var p = _menu.get_popup()
+	if p.get_item_count()==0:
+		p.add_item(tr("Help"), 1)
+		p.add_separator()
+		p.add_item(tr("About"), 2)
+		p.connect("id_pressed", self, "item_selected")
+	add_control_to_container( CONTAINER_CANVAS_EDITOR_MENU, _menu )
+	
+	return true
